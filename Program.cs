8bc331e45905b19f6bc8cc732d1017f266c5d4c6
StBarthelemy <130822796+StBarthelemy@users.noreply.github.com>
2023-05-01 14:29:59 +0300

    Сохранил 3 задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine ("Введите число");
+int points = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= points; i++ )
+{
+Console.WriteLine(i*i*i);
+}
